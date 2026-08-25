e52b095e54e0c203299e01911140a5c8bb8e058f
shpark-inco <shpark@insilicogen.com>
2026-08-25 14:11:34 +0900

    update

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Update system packages
-sudo apt-get update
-
-# Install vim and python3-pip
-sudo apt-get install -y vim python3-pip
-
-echo "Setup complete: vim and pip installed"
